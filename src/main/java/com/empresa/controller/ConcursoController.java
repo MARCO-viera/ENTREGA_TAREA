fddc04599b7aa421c63099ff5eb94abe8c9e1156
USUARIO <USUARIO@DESKTOP-C4NRIE2>
2023-09-08 19:53:18 -0500

    tarea resuelta Medicamento y Concurso

---
+++ src/main/java/com/empresa/controller/ConcursoController.java
@@ -0,0 +1,46 @@
+package com.empresa.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.empresa.entity.Concurso;
+import com.empresa.service.ConcursoService;
+
+@Controller
+public class ConcursoController {
+
+	@Autowired
+	private ConcursoService service;
+	
+	@GetMapping(value = "/verConcurso" )
+	public String ver() {
+		return "registraConcurso";
+	}
+	
+	
+	@PostMapping("/registraConcurso")
+	@ResponseBody
+	public Map<?, ?> inserta(Concurso obj){
+		HashMap<String, String> salida = new HashMap<String, String>();
+		
+		Concurso objSalida = service.insertaConcurso(obj);
+		if (objSalida == null) {
+			salida.put("MENSAJE", "Error en el registro");
+		}else {
+			salida.put("MENSAJE", "Registro exitoso!!");
+		}
+		
+		return salida;
+	}
+
+}
+
+
+
+
